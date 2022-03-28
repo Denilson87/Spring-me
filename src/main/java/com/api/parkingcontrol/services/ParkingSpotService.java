834be440849bd3b956a2d6b5cf6dc342a7fbf0ad
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-03-29 00:41:34 +0200

    First realese

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,19 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    public Object save(ParkingSpotModel parkingSpotModel) {
+        return ParkingSpotRepository.save(ParkingSpotModel);
+    }
+}
