9afa1bdbb0df71ab2dab2ee47a9fd8b62882f91e
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-10 01:39:20 +0200

    Setting the pageable to @GetMapping

--- src/main/java/com/api/parkingcontrol/configurations/DateConfigurations.java
+++ src/main/java/com/api/parkingcontrol/configurations/DateConfigurations.java
@@ -1,6 +1,5 @@
 package com.api.parkingcontrol.configurations;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
