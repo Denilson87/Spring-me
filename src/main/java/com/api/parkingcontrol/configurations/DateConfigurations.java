432104d0b2d1c3f927f4ffb301a212030f164717
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-10 01:20:41 +0200

    setting configs

--- src/main/java/com/api/parkingcontrol/configurations/DateConfigurations.java
+++ src/main/java/com/api/parkingcontrol/configurations/DateConfigurations.java
@@ -1,8 +1,20 @@
 package com.api.parkingcontrol.configurations;
 
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DateConfigurations {
+    public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(LOCAL_DATETIME_SERIALIZER);
+        return new ObjectMapper()
+                .registerModule(module);
+    }
 }
