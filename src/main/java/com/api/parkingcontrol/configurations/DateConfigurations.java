796f72dc84cc6ca5ec89bad576d51c76be3fc29a
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-10 01:23:43 +0200

    setting the configurations 4 dates
    using the ISO 8601 UTC patterns

--- src/main/java/com/api/parkingcontrol/configurations/DateConfigurations.java
+++ src/main/java/com/api/parkingcontrol/configurations/DateConfigurations.java
@@ -2,6 +2,7 @@ package com.api.parkingcontrol.configurations;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
