071f01556a2a816ddfd60e850f23784740f1a0c1
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-10 01:22:10 +0200

    imports

--- src/main/java/com/api/parkingcontrol/configurations/DateConfigurations.java
+++ src/main/java/com/api/parkingcontrol/configurations/DateConfigurations.java
@@ -4,6 +4,7 @@ package com.api.parkingcontrol.configurations;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.time.format.DateTimeFormatter;
 
