9bc8e30021f8a8862dfb3fa5e39b0df567b6dd42
Denilson <denilsonmucavel@outlook.com>
2023-08-18 15:55:30 +0200

    building services for user db transactions

---
+++ src/main/java/com/api/parkingcontrol/services/UserService.java
@@ -0,0 +1,30 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.models.UserModel;
+import com.api.parkingcontrol.repositories.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import javax.transaction.Transactional;
+
+public class UserService {
+
+    final UserRepository userRepository;
+    public UserService(UserRepository  userRepository){
+        this.userRepository = userRepository;
+    }
+    @Transactional
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+
+    public Page<UserModel> findAll(Pageable pageable) {
+        return  userRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
+}
