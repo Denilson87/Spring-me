86f7d53198c2c6732c1476ea85f9c95462d89e8e
Denilson <denilsonmucavel@outlook.com>
2023-08-18 16:59:17 +0200

    config userDetails methods

--- src/main/java/com/api/parkingcontrol/models/UserModel.java
+++ src/main/java/com/api/parkingcontrol/models/UserModel.java
@@ -2,13 +2,17 @@ package com.api.parkingcontrol.models;
 
 
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-public class UserModel implements Serializable {
+public class UserModel implements UserDetails {
 
     private static final long serialVersionUID =1L;
     @Id
@@ -36,8 +40,33 @@ public class UserModel implements Serializable {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     public String getPassword() {
