ad77a46d478b6a5201ab9e1a4cc4023ca8168789
Denilson <denilsonmucavel@outlook.com>
2023-08-23 09:32:47 +0200

    updating Authorization service

--- src/main/java/com/api/parkingcontrol/repositories/UserRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/UserRepository.java
@@ -2,10 +2,13 @@ package com.api.parkingcontrol.repositories;
 
 import com.api.parkingcontrol.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+ UserDetails findbyLogin(String username);
 
 }
